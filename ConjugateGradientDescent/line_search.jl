333e2e44ca6e72c2f0005d1c476780c2224cf25d
우준상 <ujunsang@ujunsang-ui-MacBookAir.local>
2025-05-31 11:34:46 +0900

    초기화

--- ConjugateGradientDescent/line_search.jl
+++
@@ -1,7 +0,0 @@
-include("minimize.jl")
-function line_search(f, x, d)
-    objective = α-> f(x + α*d)
-    a, b = bracket_minimum(objective)
-    α = minimize(objective, a, b)
-    return x + α*d
-end
